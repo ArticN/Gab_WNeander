4923a8e8afa99430e12321ab831d177363ea1b15
Gabriel Reimberg <gabrielreimbergdepaiva@gmail.com>
2025-04-05 23:31:16 -0300

    reset

--- src/compiler/codegen.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODEGEN_H
-#define CODEGEN_H
-
-#include <stdio.h>
-#include "ast.h"
-
-void generate_code(FILE *out, Program *program);
-
-#endif
--- src/compiler/parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "ast.h"
-
-Program *parse_lng_file(const char *filename);
-
-#endif
